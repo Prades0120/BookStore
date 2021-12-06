dc681351293d8a0a46286160f8191c5f75be561a
Prades <alexandre.prades.c@gmail.com>
2021-12-06 23:30:37 +0100

    finished

--- app/src/main/java/com/example/bookstore/ui/synopsis/SynopsisFragment.kt
+++ app/src/main/java/com/example/bookstore/ui/synopsis/SynopsisFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.bookstore.R
 import com.example.bookstore.databinding.FragmentSynopsisBinding
-import com.example.bookstore.ui.books.BooksAdapter
 
 class SynopsisFragment : Fragment() {
 
@@ -27,16 +25,34 @@ class SynopsisFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         synopsisViewModel =
-            ViewModelProvider(this).get(SynopsisViewModel::class.java)
+            ViewModelProvider(this)[SynopsisViewModel::class.java]
 
         _binding = FragmentSynopsisBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val autoComplete: AutoCompleteTextView = root.findViewById(R.id.autoCompleteTextView)
+        val synopsis: TextView = root.findViewById(R.id.textView6)
 
-       synopsisViewModel.list.observe(viewLifecycleOwner, Observer {
-
+       synopsisViewModel.list.observe(viewLifecycleOwner, {
+           val adapter = ArrayAdapter (root.context,android.R.layout.simple_dropdown_item_1line,it)
+           autoComplete.setAdapter(adapter)
+           autoComplete.threshold = 1
         })
+
+        autoComplete.setOnItemClickListener { _, _, _, _ ->
+            when (autoComplete.text.toString()) {
+                getString(R.string.morir_ines_panas) -> synopsis.setText(R.string.synopsis0)
+                getString(R.string.yo_julia) -> synopsis.setText(R.string.synopsis1)
+                getString(R.string.carlos_lopez_book) -> synopsis.setText(R.string.synopsis2)
+                getString(R.string.pasos_en_escalera) -> synopsis.setText(R.string.synopsis3)
+                getString(R.string.cementerio_animales) -> synopsis.setText(R.string.synopsis4)
+                getString(R.string.sidi) -> synopsis.setText(R.string.synopsis5)
+                getString(R.string.jaula_de_oro) -> synopsis.setText(R.string.synopsis6)
+                getString(R.string.lo_mejor_de_ir) -> synopsis.setText(R.string.synopsis7)
+            }
+        }
+
         return root
     }
 
--- app/src/main/java/com/example/bookstore/ui/synopsis/SynopsisViewModel.kt
+++ app/src/main/java/com/example/bookstore/ui/synopsis/SynopsisViewModel.kt
@@ -1,17 +1,20 @@
 package com.example.bookstore.ui.synopsis
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.bookstore.R
-import com.example.bookstore.ui.books.Book
-import kotlin.coroutines.coroutineContext
 
 class SynopsisViewModel : ViewModel() {
 
     private val listOfBooks = MutableLiveData<ArrayList<String>>().apply {
         val list = ArrayList<String>()
+        list.add("Morir no es lo que más duele")
+        list.add("Yo, Julia")
+        list.add("La vida en cuatro letras")
+        list.add("Tus pasos en la escalera")
+        list.add("Cementerio de animales")
+        list.add("Sidi")
+        list.add("Una jaula de oro")
+        list.add("Lo mejor de ir es volver")
         value = list
     }
     val list = listOfBooks
